d5c0b2c4e6183643e5923d2ce0401605eb761708
Yara Daudt <yaracrisalis@gmail.com>
2024-07-09 20:06:14 +0100

    Input icons styled

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,14 @@
 import { View, Text, Image } from "react-native"
 
+import { MapPin, Calendar as CalendarIcon} from "lucide-react-native"
+
+import { Input } from "@/components/input"
+
+import { colors } from "@/styles/colors"
+
 export default function Index() {
     return (
-        <View className="flex-1 items-center justify-center">
+        <View className="flex-1 items-center justify-center px-5">
             <Image 
                 source={require("@/assets/logo.png")}
                 className="h-8"
@@ -12,6 +18,24 @@ export default function Index() {
             <Text className="text-zinc-400 font-regular text-center text-lg mt-3">
                 Convide seus amigos e planeje sua {"\n"}próxima viagem
             </Text>
+
+            <View className="w-full bg-zinc-900 p-4 rounded-xl my-8 border border-zinc-800">
+                <Input >
+                    <MapPin color={colors.zinc[400]} size={20}/>
+                    <Input.Field placeholder="Para onde?"/>
+                </Input>
+
+                <Input >
+                    <CalendarIcon color={colors.zinc[400]} size={20}/>
+                    <Input.Field placeholder="Quando?"/>
+                </Input>
+
+                <View className="border-b py-3 border-zinc-800">
+                    
+                </View>
+
+            </View>
+
         </View>
     )
 }
